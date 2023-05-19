8ec23dec6daf1fff91db43964660c4597087f01b
Emirhan Bulut <emirhanblt9@gmail.com>
2023-05-19 09:45:00 +0300

    Genel dosya yapısı n Layered App yapısı ile oluşturuldu.

---
+++ university-managment/src/data_access/user_dao/UserDAO.java
@@ -0,0 +1,6 @@
+package data_access.user_dao;
+
+import data_access.DataAccess;
+
+public class UserDAO implements DataAccess {
+}
---
+++ university-managment/src/data_access/util_dao/UtilDAO.java
@@ -0,0 +1,6 @@
+package data_access.util_dao;
+
+import data_access.DataAccess;
+
+public class UtilDAO implements DataAccess {
+}
